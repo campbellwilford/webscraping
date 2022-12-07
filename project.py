066d22b542b8efbe4bb8744096db250956f27b9c
campbell <campbell.com>
2022-12-07 12:37:48 -0600

    project updates

--- project.py
+++ project.py
@@ -25,18 +25,32 @@ for x in range(1,6):
     print(name[2].text)
     print(current_price[3].text)
     print(change[7].text)
-
-
-
-
-
-
-
-
-
-
-
     
+
+    if name == 'Bitcoin' and current_price < 40000:
+        print(f'Bitcoin has dropped to {current_price}')
+
+    if name == 'Ethereum' and current_price < 3000:
+        print(f'Ethereum has dropped to {current_price}')
+
+'''
+
+client = Client(keys2.accountSID,keys2.authToken)
+
+TwilioNumber = '+17432026208'
+
+myCellPhone = '+18304817381'
+
+
+textmessage = client.messages.create(to=myCellPhone,from_TwilioNumber, body=current_price)
+
+
+'''
+
+
+
+
+
 
 
 
